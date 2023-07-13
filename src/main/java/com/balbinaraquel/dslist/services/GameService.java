c1035fd2eb16edcec533030ebec4b417bfdb211d
Balbina Raquel <balbina.rakell@outlook.com>
2023-07-13 20:04:26 -0300

    SQL Query and Projections

--- src/main/java/com/balbinaraquel/dslist/services/GameService.java
+++ src/main/java/com/balbinaraquel/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.balbinaraquel.dslist.dto.GameDTO;
 import com.balbinaraquel.dslist.dto.GameMinDTO;
 import com.balbinaraquel.dslist.entities.Game;
+import com.balbinaraquel.dslist.projections.GameMinProjection;
 import com.balbinaraquel.dslist.repositories.GameRepository;  
 
 @Service  //Component dependency injection (service) into the system
@@ -24,12 +25,21 @@ public class GameService {
 		
 	}
 	
+	
 	@Transactional(readOnly = true) 
 	public List<GameMinDTO> findAll(){
 	
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional(readOnly = true) 
+	public List<GameMinDTO> findByList(Long listId){
+	
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
 	}
 	
 	
