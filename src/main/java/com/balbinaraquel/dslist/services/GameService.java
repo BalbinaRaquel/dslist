d6c9e34ef58c2862f329b5dc44e2c4cf3016d16a
Balbina Raquel <balbina.rakell@outlook.com>
2023-07-13 18:38:22 -0300

    GET Game by Id

--- src/main/java/com/balbinaraquel/dslist/services/GameService.java
+++ src/main/java/com/balbinaraquel/dslist/services/GameService.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.balbinaraquel.dslist.dto.GameDTO;
 import com.balbinaraquel.dslist.dto.GameMinDTO;
-
 import com.balbinaraquel.dslist.entities.Game;
 import com.balbinaraquel.dslist.repositories.GameRepository;  
 
@@ -16,6 +17,14 @@ public class GameService {
 	@Autowired //To inject a Game Repository instance into the Game Service
 	private GameRepository  gameRepository;
 	
+	@Transactional(readOnly = true) //to ensure that it will not perform a write operation to not block the database for writing
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
+	}
+	
+	@Transactional(readOnly = true) 
 	public List<GameMinDTO> findAll(){
 	
 		List<Game> result = gameRepository.findAll();
